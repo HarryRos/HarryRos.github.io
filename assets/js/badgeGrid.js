5ba7e5d733885cf1c5c42d58739224ba03236ff7
Harry Rossides <rossides.ac@gmail.com>
2020-08-13 00:00:13 +0100

    badge sizes

--- assets/js/badgeGrid.js
+++ assets/js/badgeGrid.js
@@ -6,6 +6,6 @@ var x = document.getElementsByClassName("badgeToDecorate");
 var bgColors = ['bg-primary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info', 'bg-dark'];
 var i;
 for (i = 0; i < x.length; i++) {
-	x[i].style.fontSize = getRandomSize(15, 25)+'px';
+	x[i].style.fontSize = getRandomSize(20, 30)+'px';
 	x[i].className += ' ' +bgColors[i%bgColors.length];
 }
