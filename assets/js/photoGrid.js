866c3591c8c0a43e01c1ab056f4846f6c591fd13
Harry Rossides <rossides.ac@gmail.com>
2020-08-12 02:20:33 +0100

    Circle style fix

--- assets/js/photoGrid.js
+++ assets/js/photoGrid.js
@@ -8,7 +8,7 @@ for (var i = 0; i < 25; i++) {
 	var width = getRandomSize(200, 400);
 	var height =  getRandomSize(200, 400);
 	// allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-	allImages += '<p class="circle" align="center" style="width:'+width+';height:'+height+';"><span>Some Text</span></p>';
+	allImages += '<p class="circle" align="center" width="'+width+'px" height="'+height+'px"><span>Some Text</span></p>';
 }
 
 $('#photos').append(allImages);
