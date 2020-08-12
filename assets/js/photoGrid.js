82807d8f3d9197badbaf9633a1bc5ab665d1e2b8
Harry Rossides <rossides.ac@gmail.com>
2020-08-12 01:52:44 +0100

    Circle test

--- assets/js/photoGrid.js
+++ assets/js/photoGrid.js
@@ -8,7 +8,7 @@ for (var i = 0; i < 25; i++) {
 	var width = getRandomSize(200, 400);
 	var height =  getRandomSize(200, 400);
 	// allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-	allImages += '<div class="circle"><p>Some More '+width+'/'+height+' Text here</p></div>';
+	allImages += '<p class="circle" align="center"><span>Some Text</span></p>';
 }
 
 $('#photos').append(allImages);
