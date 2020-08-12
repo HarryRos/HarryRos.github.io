b91a2d1fc96ae935e417c4ab5892c5f4cbdde758
Harry Rossides <rossides.ac@gmail.com>
2020-08-12 22:35:08 +0100

    Cleanup

---
+++ assets/js/badgeGrid.js
@@ -0,0 +1,11 @@
+function getRandomSize(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+var x = document.getElementsByClassName("badgeToDecorate");
+var bgColors = ['bg-primary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info', 'bg-dark'];
+var i;
+for (i = 0; i < x.length; i++) {
+	x[i].style.fontSize = getRandomSize(15, 25)+'px';
+	x[i].className += ' '+bgColors[getRandomSize(0, bgColors.length)];
+}
