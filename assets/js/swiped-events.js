d461eb88ac2a2478b67cc15e7ba5e0ba93b18aa8
Harry Rossides <rossides.ac@gmail.com>
2020-08-18 23:06:48 +0100

    Swipe test

--- assets/js/swiped-events.js
+++ assets/js/swiped-events.js
@@ -110,17 +110,13 @@
 
 document.addEventListener('swiped-left', function(e) {
 	console.log(e.target); // the element that was swiped
-	var x = document.getElementsByClassName("badgeToDecorate");
-	// var bgColors = ['bg-primary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info', 'bg-dark'];
-	var bgColors = ['bg-info', 'bg-success', 'bg-primary', 'bg-danger', 'bg-warning', 'bg-dark'];
-	var sizes = [22, 26, 30];
-	var i;
-	for (i = 0; i < x.length; i++) {
-		x[i].style.fontSize = sizes[getRandomSize(0, sizes.length-1)]+'px';
-		x[i].className += ' ' +bgColors[i%bgColors.length];
-	};
+	document.getElementsByClassName("main-nav");
 });
 
 document.addEventListener('swiped-right', function(e) {
-    console.log(e.target); // the element that was swiped
+	var navBar = document.getElementById("main-nav");
+	var current = navBar.getElementsByClassName("active")[0];
+	var next = current.nextSibling;
+	current.className = current.className.replace("active", "");
+	next.className  += " active";
 });
