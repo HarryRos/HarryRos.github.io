dbe5b740707ec96788e72d6c8165a6ee85d70b68
Harry Rossides <rossides.ac@gmail.com>
2020-08-27 20:42:23 +0100

    Mathjax allow parentheses

--- assets/js/badgeGrid.js
+++ assets/js/badgeGrid.js
@@ -8,7 +8,8 @@ var bgColors = ['bg-info', 'bg-success', 'bg-primary', 'bg-danger', 'bg-warning'
 var sizes = [22, 26, 30];
 var i;
 for (i = 0; i < x.length; i++) {
-	x[i].style.fontSize = sizes[getRandomSize(0, sizes.length-1)]+'px';
+	x[i].style.fontSize = '22px';
 	x[i].className += ' bg-dark';
+	// x[i].style.fontSize = sizes[getRandomSize(0, sizes.length-1)]+'px';
 	// x[i].className += ' ' +bgColors[i%bgColors.length];
 }
